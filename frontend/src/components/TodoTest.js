95ad512cd8a9e725394117eb2940c7d9804fb8fb
santhoshnov <shirobro009@gmail.com>
2023-07-31 17:26:39 +0530

    axios-setup

--- frontend/src/components/TodoTest.js
+++ frontend/src/components/TodoTest.js
@@ -1,13 +1,93 @@
 //This code is for test purpose
 
-import React, { useState } from 'react';
+import React from 'react';
 import { getTodos,addTodo,deleteTodo,editTodo } from '../api/api';
 
 function TodoTest(){
-    const [todos,setTodo] = React.useState([]);
+    const [todos,setTodos] = React.useState([]);
     const [newTodo,setNewTodo] = React.useState('');
-    const [editIndex, setEditIndex] = useState(null);
-    const [editTodo,setEditTodo] = useState('');
+    const [editIndex, setEditIndex] = React.useState(null);
+    const [editedTodo,setEditedTodo] = React.useState('');
+
+    React.useEffect(()=>{
+        fetchTodos();
+    })
+
+    const fetchTodos = async()=>{
+        try {
+            const response = await getTodos();
+            setTodos(response.data);
+          } catch (error) {
+            console.error('Error fetching todos:', error);
+          } 
+    };
+
+    const handleAddTodo = async () => {
+        try {
+          await addTodo(newTodo);
+          setNewTodo('');
+          fetchTodos();
+        } catch (error) {
+          console.error('Error adding todos:', error);
+        }
+      };
+
+    const handleDeleteTodo = async(index)=>{
+        try {
+            await deleteTodo(index);
+            fetchTodos();
+          } catch (error) {
+            console.error('Error deleting todo:', error);
+          }
+    };
+
+    const handleEditTodo = async()=>{
+        try {
+            await editTodo(editIndex, editedTodo);
+            setEditIndex(null);
+            setEditedTodo('');
+            fetchTodos();
+          } catch (error) {
+            console.error('Error updating todo:', error);
+          }
+    };
+
+    return (
+        <div>
+        <h2>Todo List</h2>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              {editIndex === index ? (
+                <>
+                  <input
+                    type="text"
+                    value={editedTodo}
+                    onChange={(e) => setEditedTodo(e.target.value)}
+                  />
+                  <button onClick={handleEditTodo}>Save</button>
+                </>
+              ) : (
+                <>
+                  {todo}
+                  <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+                  <button onClick={() => {
+                    setEditIndex(index);
+                    setEditedTodo(todo);
+                  }}>Edit</button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div> 
+    )
 
 }
 
