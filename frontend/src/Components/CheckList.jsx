4ad26a9225948377613d08e21d7642e37738b613
sudharaathith <t.sudharaathith@gmail.com>
2023-08-08 20:09:50 +0530

    fixed the deleting bug

--- frontend/src/Components/CheckList.jsx
+++ frontend/src/Components/CheckList.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CheckList(props) {
   return (
     <div className='my-2 bg-white shadow-sm w-full py-2 rounded-lg px-4 cursor-pointer flex flex-row'>
-        <input type='checkbox' onChange={()=>props.onChecked(props.key)}/>
+        <input type='checkbox' onChange={props.onChecked} checked={false}/>
         <input className=' ml-3 focus:outline-none w-full' type='text' value={props.value} onChange={props.onType} />
     </div>
   )
--- frontend/src/Page/ToDoList.jsx
+++ frontend/src/Page/ToDoList.jsx
@@ -1,22 +1,43 @@
-import React, { useState } from 'react'
-import Continer from '../Components/Continer'
-import CheckList from '../Components/CheckList'
-import { useParams } from 'react-router';
+import React, { useState } from "react";
+import Continer from "../Components/Continer";
+import CheckList from "../Components/CheckList";
+import { useParams } from "react-router";
 
 function ToDoList() {
-    const [toDoList, setToDoList] = useState([]);
-    const {list} = useParams();
-
+  const [toDoList, setToDoList] = useState([]);
+  const { list } = useParams();
 
   return (
-    <div className='w-screen h-screen flex'>
-    <Continer name={list} className="m-auto select-none" onAdd={()=>setToDoList([...toDoList, ''])} onEdit={()=>console.log("Added")} onDelete={()=>console.log("Added")}>
-        {   
-            toDoList.map((item, index)=>(<CheckList onChecked={(c)=>{let temp = [...toDoList];temp.pop(index);setToDoList(temp);console.log(index)}} key={index} />))
-        }
-    </Continer>    
-</div>
-  )
+    <div className="w-screen h-screen flex">
+      <Continer
+        name={list}
+        className="m-auto select-none"
+        onAdd={() => setToDoList([...toDoList, ""])}
+        onEdit={() => console.log("Added")}
+        onDelete={() => console.log("Added")}
+      >
+        {toDoList.map((item, index) => (
+          <CheckList
+            value={item}
+            onType={(e) => {
+              setToDoList([
+                ...toDoList.slice(0, index),
+                e.target.value,
+                ...toDoList.slice(index + 1),
+              ]);
+            }}
+            onChecked={(c) => {
+              let temp = [...toDoList];
+              temp.splice(index, 1);
+              setToDoList(temp);
+              console.log(c.target.value);
+            }}
+            key={index}
+          />
+        ))}
+      </Continer>
+    </div>
+  );
 }
 
-export default ToDoList
+export default ToDoList;
